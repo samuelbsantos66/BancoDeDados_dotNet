14abcbb51eb382873d802f2ca960ce89bba4ed1c
Samuel Barbosa Santos <125471851+samuelbsantos66@users.noreply.github.com>
2024-10-31 08:40:36 -0300

    Add files via upload

---
+++ Introducao_BD/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+using Microsoft.Data.SqlClient;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        SqlConnection conexao;
+        conexao = new("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=MyUniversidadeDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        conexao.Open();
+        Console.WriteLine("Conexão OK");
+        Console.WriteLine(" == Salvando no banco de dados ==");
+        var insertCmd = conexao.CreateCommand();
+        insertCmd.CommandText = "INSERT INTO Cursos (nome, categoria, periodo) VALUES (@nome, @categoria, @periodo)";
+
+        var paramNome = new SqlParameter("@nome","POO");
+        insertCmd.Parameters.Add(paramNome);
+
+        var paramCategoria = new SqlParameter("@categoria", "Maneiro");
+        insertCmd.Parameters.Add(paramCategoria);
+
+        var paramPeriodo = new SqlParameter("@periodo", 4);
+        insertCmd.Parameters.Add(paramPeriodo);
+
+        insertCmd.ExecuteNonQuery();
+
+        conexao.Close();
+    }
+}
